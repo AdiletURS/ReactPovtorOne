1cf3a21c3f5fbc1b46253c89dd4866aba977ed48
Адилет <110529980+AdiletURS@users.noreply.github.com>
2023-01-17 17:29:07 +0600

    firstHw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function PortfolioPage() {
+  return (
+    <>
+      <Title title="PortfolioPage" />
+    </>
+  );
+}
+
+function Title(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PortfolioPage />
     </div>
   );
 }
